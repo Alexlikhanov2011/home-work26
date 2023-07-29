5c9c87c58decc21e6d2b4150e87dfc1b52483630
Alexlikhanov2011 <Likhanov-2011@yandex.ru>
2023-07-29 18:30:37 +0300

    дз выполнено

--- src/main/java/com/example/homework26/service/EmloyeeService.java
+++ src/main/java/com/example/homework26/service/EmloyeeService.java
@@ -6,48 +6,46 @@ import com.example.homework26.exception.EmployeeStorageIsFullException;
 import com.example.homework26.model.Employee;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class EmloyeeService {
     private static final int SIZE = 10;
-    private final List<Employee> employees = new ArrayList<>();
+    private final Map<String, Employee> employees = new HashMap<>();
 
     public void addEmployee(String lastName, String firstName) {
         if (employees.size() == SIZE) {
             throw new EmployeeStorageIsFullException();
         }
-        var employee = new Employee(lastName, firstName);
-        if (employees.contains(employee)) {
+        var key = makeKey(lastName,firstName);
+        if (employees.containsKey(key)) {
             throw new EmployeeAllreadyAddedException();
         }
-        employees.add(employee);
+        var employee = new Employee(lastName, firstName);
+        employees.put(key, employee);
     }
 
     public Employee findEmployee(String lastName, String firstName) {
-        var employee = new Employee(lastName, firstName);
-        for (Employee emp : employees) {
-            if (emp.equals(employee)) {
-                return emp;
-            }
-        }
-        throw new EmployeeNotFoundException();
+      var emp = employees.get(makeKey(lastName,firstName));
+      if (emp == null){
+          throw new EmployeeNotFoundException();
+      }
+      return emp;
     }
 
     public boolean removeEmployee(String lastName, String firstName) {
+        Employee removed = employees.remove(makeKey(lastName,firstName));
         var employee = new Employee(lastName, firstName);
-        for (Employee e : employees) {
-            if (e.equals(employee)) {
-                employees.remove(e);
-                return true;
-            }
+        if (removed == null){
+            throw new EmployeeNotFoundException();
         }
-        throw new EmployeeNotFoundException();
+        return true;
     }
 
     public Collection<Employee> getAll() {
-        return employees;
+        return employees.values();
+    }
+    private String makeKey(String lastName, String firstName){
+        return (lastName + "_" + firstName).toLowerCase();
     }
 }
